fcebf2de9bfd6ba5600e7f0d5861363d70368723
Abstract Programmer <Abstract Programmer>
2025-07-18 19:31:29 +0100

    backup

--- client/src/pages/EmailDesigner.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import EmailDesigner from '@/components/EmailDesigner';
-
-const EmailDesignerPage: React.FC = () => {
-  return <EmailDesigner />;
-};
-
-export default EmailDesignerPage;
